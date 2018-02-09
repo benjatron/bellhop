dff6b402480d3fcf66426a2f05e01e71ec5aa23d
Ben Davidson <ben@bendavidson.me>
2018-02-09 18:01:53 -0500

    Removed bower_components support because we don't use Bower

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
       loaders: [
         {
           test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
             options: {
