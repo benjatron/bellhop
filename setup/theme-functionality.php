eb0095b8d00cc6798338626d8bc1bd3528c84b43
Ben Davidson <ben@bendavidson.me>
2018-04-27 13:13:37 -0400

    Updated basic functionality functions and settings

--- setup/theme-functionality.php
+++ setup/theme-functionality.php
@@ -1,17 +1,11 @@
 <?php
-
 function bellhop_supports() {
 // Make theme available for translation
-// Community translations can be found at https://github.com/roots/sage-translations
 load_theme_textdomain('bellhop', get_template_directory() . '/lang');
 
 // Enable plugins to manage the document title
 // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
 add_theme_support('title-tag');
-
-// Enable post formats
-// http://codex.wordpress.org/Post_Formats
-add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
 
 // Enable HTML5 markup support
 // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
@@ -22,9 +16,18 @@ add_action( 'after_setup_theme', 'bellhop_supports' );
 
 // Removes unnecessary items from the admin menu
 function bellhop_remove_menus() {
-  // remove_menu_page('edit.php');
-  // remove_menu_page('edit-comments.php');
-  remove_menu_page('link-manager.php');
+  // remove_menu_page( 'index.php' );                  //Dashboard
+  // remove_menu_page( 'jetpack' );                    //Jetpack*
+  // remove_menu_page( 'edit.php' );                   //Posts
+  // remove_menu_page( 'upload.php' );                 //Media
+  // remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  // remove_menu_page( 'themes.php' );                 //Appearance
+  // remove_menu_page( 'plugins.php' );                //Plugins
+  // remove_menu_page( 'users.php' );                  //Users
+  // remove_menu_page( 'tools.php' );                  //Tools
+  // remove_menu_page( 'options-general.php' );        //Settings
+  remove_menu_page( 'link-manager.php' );           //Links
 }
 add_action( 'admin_menu', 'bellhop_remove_menus');
 
@@ -53,7 +56,37 @@ if( function_exists('acf_add_options_page') ) {
 
 }
 
+
+// Removes ACF options from child sites
+if( is_multisite() && (get_current_blog_id() != 1) ):
+  add_filter('acf/settings/show_admin', '__return_false');
+endif;
+
+
+// Changes the save location for ACF fields
+add_filter('acf/settings/save_json', 'bellhop_acf_json_save_point');
+function bellhop_acf_json_save_point( $path ) {
+  $path = get_stylesheet_directory() . '/assets/json/acf';
+  return $path;
+}
+
+
+// Changes the load location for ACF fields
+add_filter('acf/settings/load_json', 'bellhop_acf_json_load_point');
+function bellhop_acf_json_load_point( $paths ) {
+  unset($paths[0]);
+  $paths[] = get_stylesheet_directory() . '/assets/json/acf';
+  return $paths;
+}
+
+
 // Simplified partials integration for cleaner markup
 function get_partial( $slug ) {
   return include(locate_template('partials/' . $slug . '.php'));
+}
+
+
+// SVG importer
+function get_svg( $file ) {
+  echo file_get_contents( get_stylesheet_directory_uri() . '/assets/images/dist/svg/' . $file . '.svg');
 }
